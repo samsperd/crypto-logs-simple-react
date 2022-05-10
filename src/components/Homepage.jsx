5cd8cbdaca0ae0d05abe7fce931871d2c9f538d4
samsperd <iloejeobinna@gmail.com>
2022-05-10 20:24:53 +0100

    final commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -14,56 +14,67 @@ const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
+  let information;
 
-  if (isFetching) return <Loader></Loader>;
-  
+  if (isFetching) {
+    return <Loader></Loader>;
+  }
+  else {
+    if (!globalStats) {
+      information = (<Loader></Loader>);
+    } else {
+      information = (
+        <>
+          <Title level={2} className="heading">
+            Global Crypto Stats
+          </Title>
+          <Row>
+            <Col span={12}>
+              <Statistic title="Total Cryptocurrencies" value={globalStats.total} ></Statistic>
+            </Col>
+            <Col span={12}>
+              <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} ></Statistic>
+            </Col>
+            <Col span={12}>
+              <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} ></Statistic>
+            </Col>
+            <Col span={12}>
+              <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} ></Statistic>
+            </Col>
+            <Col span={12}>
+              <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} ></Statistic>
+            </Col>
+          </Row>
+          <div className="home-heading-container">
+            <Title level={2} className="home-title" >
+              Top 10 Cryptocurrencies in the world
+            </Title>
+            <Title level={3} className="show-more" >
+              <Link to="/cryptocurrencies">
+                Show More
+              </Link>
+            </Title>
+          </div>
+          <Cryptocurrencies simplified></Cryptocurrencies>
+          <div className="home-heading-container">
+            <Title level={2} className="home-title" >
+              Latest Crypto News
+            </Title>
+            <Title level={3} className="show-more" >
+              <Link to="/news">
+                Show More
+              </Link>
+            </Title>
+          </div>
+          <News simplified></News>
+        </>
+      );
+    }
+  }
 
   return (
-    <>
-      <Title level={2} className="heading">
-        Global Crypto Stats
-      </Title>
-      <Row>
-        <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value={globalStats.total} ></Statistic>
-        </Col>
-        <Col span={12}>
-          <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} ></Statistic>
-        </Col>
-        <Col span={12}>
-          <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} ></Statistic>
-        </Col>
-        <Col span={12}>
-          <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} ></Statistic>
-        </Col>
-        <Col span={12}>
-          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} ></Statistic>
-        </Col>
-      </Row>
-      <div className="home-heading-container">
-        <Title level={2} className="home-title" >
-          Top 10 Cryptocurrencies in the world
-        </Title>
-        <Title level={3} className="show-more" >
-          <Link to="/cryptocurrencies">
-            Show More
-          </Link>
-        </Title>
-      </div>
-      <Cryptocurrencies simplified></Cryptocurrencies>
-      <div className="home-heading-container">
-        <Title level={2} className="home-title" >
-          Latest Crypto News
-        </Title>
-        <Title level={3} className="show-more" >
-          <Link to="/news">
-            Show More
-          </Link>
-        </Title>
-      </div>
-      <News simplified></News>
-    </>
-  )
+    { information }
+  );
 }
 
 export default Homepage
